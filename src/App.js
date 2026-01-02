3b1e89dcd36f01cd179c314045c3e5484d8c9571
Sesandu <sesanduj@gmail.com>
2026-01-02 22:20:55 +0530

    feat(ui): Navigation Bar Created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from './pages/Homepage';
 
 function App() {
   return (
-    <div className="App">
-      <Homepage/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 
 const Homepage = () => {
   return (
-    <div>
+    <div className="bg-background-light dark:bg-background-dark text-slate-900 dark:text-white font-display overflow-x-hidden antialiased selection:bg-primary selection:text-white overscroll-none">
       <Navbar/>
     </div>
   )
