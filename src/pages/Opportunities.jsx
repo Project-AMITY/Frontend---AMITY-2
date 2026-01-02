a74fee0a457799b025161caf2d3e0336dc270749
Sesandu <sesanduj@gmail.com>
2026-01-02 23:57:27 +0530

    feat(ui): Opportunities page completed

--- src/pages/Opportunities.jsx
+++ src/pages/Opportunities.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import EventCards from "../Components/EventCards";
+import EventFilter from "../Components/EventFilter";
+import EventData from "../Components/EventData";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
-const Opportunities = () => {
+
+const Oppotunities = () => {
   return (
-    <div>
-      This is opportunity page
+    <div className="bg-background-light dark:bg-background-dark text-slate-900 dark:text-white font-display overflow-x-hidden antialiased selection:bg-primary selection:text-white">
+      <Navbar/>
+      <div className="pt-20">
+        <EventFilter/>
+      </div>
+      <EventData/>
+      <Footer/>
     </div>
-  )
-}
+  );
+};
 
-export default Opportunities
+export default Oppotunities;
