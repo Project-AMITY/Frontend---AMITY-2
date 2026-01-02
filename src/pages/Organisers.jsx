f98d73f81845949fd40ef755e5f136f00b8f7be0
kaanthaekanayake <kaanthaekanayake.personal@gmail.com>
2026-01-03 00:09:17 +0530

    create organizer page and its components

--- src/pages/Organisers.jsx
+++ src/pages/Organisers.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
+import JoinOrganiser from "../components/JoinOrganiser";
+import OrganiserCard from "../components/OrganiserCard";
+import Footer from "../components/Footer";
 
 const Organisers = () => {
   return (
-    <div>
-      This is organiser page
+    <div className="bg-background-light dark:bg-background-dark text-slate-900 dark:text-white font-display overflow-x-hidden antialiased selection:bg-primary selection:text-white">      
+        <Navbar />
+        <JoinOrganiser />
+        <OrganiserCard />
+        <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Organisers
+export default Organisers;
