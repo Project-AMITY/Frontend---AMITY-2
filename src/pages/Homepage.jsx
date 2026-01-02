97bcaa10231f88dd45a3c1b71a82ca00b8391606
Sesandu <sesanduj@gmail.com>
2026-01-02 23:06:22 +0530

    feat(ui): added Homepage Event cards to the Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import HomepageHero from '../components/HomepageHero'
+import Footer from '../components/Footer'
+import HomePageEventCard from '../components/HomepageEventCard'
 
 const Homepage = () => {
   return (
     <div className="bg-background-light dark:bg-background-dark text-slate-900 dark:text-white font-display overflow-x-hidden antialiased selection:bg-primary selection:text-white overscroll-none">
       <Navbar/>
       <HomepageHero/>
+      <HomePageEventCard/>
+      <Footer/>
     </div>
   )
 }
