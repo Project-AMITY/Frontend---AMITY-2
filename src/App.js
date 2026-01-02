6707d94687cc19c84d8a924d9401bdd500d4d4a8
Sesandu <sesanduj@gmail.com>
2026-01-03 04:27:54 +0530

    Added organiser register form path to App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import OrgReg from "./pages/OrgRegister";
 import UserProfile from "./pages/UserProfile";
 import OrganiserProfile from "./pages/OrgProfile";
 import CreateNewEvent from "./pages/CreateNewEvents";
+import OrgRegForm from "./components/OrgRegForm";
 
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
           <Route path="/user/profile" element={<UserProfile/>} />
           <Route path="/organiser/profile" element={<OrganiserProfile/>} />
           <Route path="/organiser/create" element={<CreateNewEvent/>} />
+          <Route path="/register/orgreg" element={<OrgRegForm/>} />
         </Routes>
       </div>
     </Router>
