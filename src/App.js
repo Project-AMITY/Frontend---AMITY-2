409448070375c24231b2657f8f47f55a4f0411f4
kaanthaekanayake <kaanthaekanayake.personal@gmail.com>
2026-01-03 02:10:12 +0530

    Event card link set. Now those are clickable and navigate to event full detail component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from './pages/Homepage';
-import Opportunities from './pages/Opportunities'
-import Organisers from './pages/Organisers'
-import Login from './pages/Login';
-import Register from './pages/Register';
-import EventDetails from './pages/EventDetails';
-
-
-import OrgProfile from './pages/OrgProfile';
-import OrgRegister from './pages/OrgRegister';
+import Homepage from "./pages/Homepage";
+import Opportunities from "./pages/Opportunities";
+import Organisers from "./pages/Organisers";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import EventDetails from "./pages/EventDetails";
+import OrgReg from "./pages/OrgRegister";
+import UserProfile from "./pages/UserProfile";
+import OrganiserProfile from "./pages/OrgProfile";
+import CreateNewEvent from "./pages/CreateNewEvents";
 
 
 function App() {
@@ -19,12 +18,15 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/opportunities" element={<Opportunities/>}/>
-          <Route path='/organisers' element={<Organisers/>}/>
-          {/* <Route path='/organisers' element={<OrgProfile/>}/> */}
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register'element={<Register/>}/>
-          <Route path='/register/orgreg'element={<OrgRegister/>}/>
+          <Route path="/opportunities" element={<Opportunities />} />
+          <Route path="/organisers" element={<Organisers />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/event/:id" element={<EventDetails/>} />
+          <Route path="/organisers/login" element={<OrgReg/>} />
+          <Route path="/user/profile" element={<UserProfile/>} />
+          <Route path="/organiser/profile" element={<OrganiserProfile/>} />
+          <Route path="/organiser/create" element={<CreateNewEvent/>} />
         </Routes>
       </div>
     </Router>
--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import EventFullDetails from '../components/EventFullDetails'
+import EventFullDetail from '../components/EventFullDetails'
 import Footer from '../components/Footer'
-
+import { useParams } from 'react-router-dom'; // Hook to grab URL parameters
 
 const EventDetails = () => {
+
+  // 1. Capture the 'id' from the URL (e.g., /event/3)
+  const { id } = useParams();
+
   return (
     <div className="bg-background-light dark:bg-background-dark text-slate-900 dark:text-white font-display overflow-x-hidden antialiased selection:bg-primary selection:text-white">
-      <Navbar/>
+      <Navbar />
       <main className="flex-grow pt-6 pb-20">
         <div className="pt-20">
-            <EventFullDetails/>
+            <EventFullDetail eventId={id}/>
         </div>
       </main>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
